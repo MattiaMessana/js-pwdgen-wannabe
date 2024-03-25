3e231f89fb928de1c61e58ece52f232fa5d90af3
MattiaMessana <mattia.messana@icloud.com>
2024-03-25 12:07:17 +0100

    add:fix error

--- js/script.js
+++ js/script.js
@@ -4,18 +4,19 @@
 // Chiediamo dei dati all'utente per generare una password 
 
 
+
 // BLOCCO DI RACCOLTA DEI DATI 
-// con un propt chiediamo all'utente il suo nome
-let userName = prompt("Ciao user! Quale è il tuo nome?"); //string
+// con un propt chiediamo all'utente il suo nome   //quando chiedo dati all'utente di solito sono dati che non voglio cambiare quindi uso const
+const userName = prompt("Ciao user! Quale è il tuo nome?"); //string | null
 console.log(userName, typeof userName);
 // con un propt chiediamo all'utente il suo cognome 
-let userSurename = prompt("Ora dimmi il tuo cognome?"); //string
+const userSurename = prompt("Ora dimmi il tuo cognome?"); //string | null
 console.log(userSurename, typeof userSurename);
 // con un propt chiediamo all'utente il suo colore preferito 
-let userColor = prompt("Quale è il tuo colore preferito?"); //string
+const userColor = prompt("Quale è il tuo colore preferito?"); //string | null
 console.log(userColor, typeof userColor);
-//con math.random generiamo un numero casuale compreso fra 1 a 100 inclusi 
-let numberCasual = Math.floor((Math.random() * 100) + 1); //number
+//con math.random generiamo un numero casuale decimale compreso fra 0 a 100 (inclusi) con math.floor genera numeri interi 
+const numberCasual = Math.floor(Math.random() * 101); //number
 console.log(numberCasual, typeof numberCasual);
 
 
